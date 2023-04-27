3122a8c4d8aa6ee7b299d8ebcf0813269b24a065
aashutoshtaikar <aashutoshtaikar@gmail.com>
2023-04-26 21:44:11 -0400

    adding nothing app

---
+++ src/main/java/com/ex/na/NothingAppApplication.java
@@ -0,0 +1,13 @@
+package com.ex.na;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NothingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NothingAppApplication.class, args);
+	}
+
+}
